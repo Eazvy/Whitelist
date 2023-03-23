67d5af1e3ecebb01c9319f476c106bf446c3cdc6
Eazvy <57739965+Eazvy@users.noreply.github.com>
2023-03-23 13:21:46 -0400

    Create GetData.lua

---
+++ GetData.lua
@@ -0,0 +1,4 @@
+_G.GlobalData = {"dc7ae61fb2274e593aa004fea3c4864a"}
+_G.DeadEnv = {"doiiawdiawidzkdk129139ZKXCKAKDQ"}
+_G.WhitelistKeys = loadstring(game:HttpGet("https://raw.githubusercontent.com/Eazvy/Whitelist/main/WhitelistData.lua"))()
+return _G.WhitelistKeys
